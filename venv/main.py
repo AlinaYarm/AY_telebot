80573832f9804f343a3ba799b20b0a580a8d879f
ayvm <alina00802@gmail.com>
2022-09-26 23:48:20 +0400

    #1 Создана часть бота

--- venv/main.py
+++ venv/main.py
@@ -1,9 +1,11 @@
 import telebot
 import os
+import random
+
+from telebot import types
 
 TOKEN = os.getenv('TELE_TOKEN')
 bot = telebot.TeleBot(TOKEN)
-
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
@@ -11,8 +13,21 @@ def start_message(message):
     # start_btn = telebot.types.KeyboardButton("/start")
     help_btn = telebot.types.KeyboardButton("/help")
     markup.add(help_btn)
-    bot.send_message(message.chat.id, 'Привет! Это супер классный бот!', reply_markup=markup)
+    bot.send_message(message.chat.id, 'Привет!', reply_markup=markup)
 
+markup = types.ReplyKeyboardMarkup(resize_keyboard = True)
+item1 = types.KeyboardButton('Нажми на кнопку, и я тебе отправлю имя твоего собеседника')
+
+markup.add(item1)
+
+companion = ['Аня', 'Павел', 'Анастасия', 'Григорий', 'Азамат', 'Денис', 'Алия']
+
+@bot.message_handler(content_types=['text', 'photo'])
+def messagelist(message):
+    if massage.text == 'Нажми на кнопку, и я тебе отправлю имя твоего собеседника':
+        bot.send_message(massage.chat.id, random.choice(companion))
+    else:
+        bot.send_message(massage.chat.id, 'Ничего не понял')
 
 @bot.message_handler(commands=['help'])
 def help_message(message):
@@ -23,7 +38,6 @@ def help_message(message):
 def get_text_messages(message):
     msg = message.text
     bot.send_message(message.from_user.id, msg)
-
 
 
 bot.polling(none_stop=True, interval=0)
