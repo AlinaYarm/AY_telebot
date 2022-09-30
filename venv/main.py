ce4f8aba61d54ec14942bab950d21f99da0be295
ayvm <alina00802@gmail.com>
2022-09-30 21:16:29 +0400

    #1 Исправлена ошибка с кнопкой ☕️

--- venv/main.py
+++ venv/main.py
@@ -13,28 +13,25 @@ companion = ['Аня', 'Павел', 'Анастасия', 'Григорий', '
 def start(message):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     btn1 = types.KeyboardButton("👋 Поздороваться")
-    btn2 = types.KeyboardButton("Начать чаепитие")
+    btn2 = types.KeyboardButton("Начать чаепитие ☕️")
     help_btn = telebot.types.KeyboardButton("/help")
     markup.add(btn1, btn2, help_btn)
-    bot.send_message(message.chat.id,
-                     text="Привет, {0.first_name}! Я бот, который рандомно выберет тебе собеседника для совместного чаепития".format(
+    bot.send_message(message.chat.id, text="Привет, {0.first_name}! Я бот, который рандомно выберет тебе собеседника для совместного чаепития".format(
                          message.from_user), reply_markup=markup)
 
 @bot.message_handler(content_types=['text'])
 def func(message):
     if (message.text == "👋 Поздороваться"):
         bot.send_message(message.chat.id, text="Привеет.. Спасибо что ты с нами!)")
-    elif (message.text == "Начать чаепитие"):
+    elif (message.text == "Начать чаепитие ☕️"):
         markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
         tea = telebot.types.KeyboardButton("Пора пить чай!")
-        markup.add(tea)
-        bot.send_message(message.chat.id, 'Нажми на кнопку "Пора пить чай!", и я тебе отправлю имя твоего собеседника!',
+        start = telebot.types.KeyboardButton("/start")
+        markup.add(tea, start)
+        bot.send_message(message.chat.id, 'Нажми кнопку "Пора пить чай!", и я тебе отправлю имя твоего собеседника! \n А если хочешь вернуться в меню нажми "start"',
                          reply_markup=markup)
     elif (message.text == "Пора пить чай!"):
-        if message.text == 'Пора пить чай!':
-            bot.send_message(message.chat.id, random.choice(companion))
-        else:
-            bot.send_message(message.chat.id, 'Ничего не понял')
+        bot.send_message(message.chat.id, random.choice(companion))
 
     elif (message.text == "/help"):
         markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
@@ -48,24 +45,18 @@ def func(message):
         bot.send_message(message.chat.id, "У меня нет имени..")
 
     elif message.text == "Что я могу?":
-        bot.send_message(message.chat.id, text="Выбрать тебе собеседника")
+        bot.send_message(message.chat.id, text="Выбрать тебе собеседника ☕️")
 
     elif (message.text == "Вернуться в главное меню"):
         markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
         button1 = types.KeyboardButton("👋 Поздороваться")
-        button2 = types.KeyboardButton("/help")
-        markup.add(button1, button2)
+        button2 = types.KeyboardButton("Начать чаепитие ☕️")
+        button3 = types.KeyboardButton("/help")
+        markup.add(button1, button2, button3)
         bot.send_message(message.chat.id, text="Вы вернулись в главное меню", reply_markup=markup)
     else:
         bot.send_message(message.chat.id, text="На такую комманду я не запрограммирован..")
 
-
-#@bot.message_handler(content_types=['text', 'photo'])
-#def messagelist(message):
-    #if message.text == 'Пора пить чай!':
-        #bot.send_message(message.chat.id, random.choice(companion))
-    #else:
-        #bot.send_message(message.chat.id, 'Ничего не понял')
 
 bot.polling(none_stop=True)
 
